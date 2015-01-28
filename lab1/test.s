9a0368e0651ee2acf96531afe4b88319d9afb9f2
Ryan Singzon <rcsingzon@gmail.com>
2015-01-28 13:56:11 -0800

    Initial project files

---
+++ lab1/test.s
@@ -0,0 +1,20 @@
+	AREA test, CODE, READONLY
+	EXPORT myfunction
+myfunction
+	
+	; Start Kalman filter assembly
+	; Pointer to the input data array
+				 
+				 
+	; Pointer to the filtered data array
+			 
+	; Array length
+			 
+	; Pointer to the Kalman filter state (this is  a struct)
+	
+	
+	BX LR ;
+	END
+		
+		
+	
