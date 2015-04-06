9112b6ce4a7a548a86cac6f569d12aa48c5f0ff1
Ryan Singzon <rcsingzon@gmail.com>
2015-04-06 13:54:47 -0400

    Removed commented code

--- spi/src/main.c
+++ spi/src/main.c
@@ -27,11 +27,7 @@ int main (void) {
 
 	uint8_t readByte;
 	uint8_t numBytes = 1;
-	
-	//statusByte = CC2500_Write(0, FLUSH_RX_FIFO_COMMAND, 0);
-	
-	uint8_t buffer[64];
-	
+		
 	statusByte = CC2500_Start_Receive();
 	
 	while((statusByte & 0xF0) != 0x10){
@@ -67,31 +63,4 @@ int main (void) {
 		}
 		
 	}
-	
-	/*
-		
-	// Read 
-	statusByte = CC2500_Read(&readByte, 0xFB, 2);
-	uint8_t bytesToRead = 0x7F & readByte;
-	
-	uint8_t receivedByte;
-	uint8_t receivedString[64];
-	
-	statusByte = CC2500_Read(receivedString, RX_FIFO_BYTE_ADDRESS, bytesToRead);
-	CC2500_Read(&readByte, 0xFB, 2);
-	
-	printf("Received String: %s\n", receivedString);
-	*/
-	/*
-		while(bytesToRead > 0){
-			statusByte = CC2500_Read(&receivedByte, RX_FIFO_BYTE_ADDRESS, 1);
-			bytesToRead--;
-			
-			CC2500_Read(&readByte, 0xFB, 2);
-		}
-	*/
-	
-	// The number of byte available to read is contained in the last
-	// four bits of the status byte
-	// Mask the status byte
 }
