f1777a456050e18d61dbbc27bd9c2325c250e296
Jeffrey Tichelman <jeffrey.tichelman@mail.mcgill.ca>
2015-04-06 20:19:30 -0400

    added servo files to receiver project, created receive threads

--- receiver/src/main.c
+++ receiver/src/main.c
@@ -10,11 +10,43 @@
 
 #include "spi.h"
 #include "cc2500.h"
+#include "servo.h"
+
+#define LEFT_MOTOR_FLAG 0x01
+#define RIGHT_MOTOR_FLAG 0x01
+
+uint32_t leftMotorDelay;
+uint32_t rightMotorDelay;
+
+// Thread prototypes
+void receiverThreadDef(void const *argument);
+void leftMotorThreadDef(void const *argument);
+void rightMotorThreadDef(void const *argument);
+
+// Thread declarations
+osThreadDef(receiverThreadDef, osPriorityNormal, 1, 0);
+osThreadDef(leftMotorThreadDef, osPriorityNormal, 1, 0);
+osThreadDef(rightMotorThreadDef, osPriorityNormal, 1, 0);
+
+
+osThreadId receiverThread;
+osThreadId leftMotorThread;
+osThreadId rightMotorThread;
+
+// Timer declarations
+void leftMotorCallback(void const *argument);
+osTimerDef (leftMotorDef, leftMotorCallback);
+osTimerId leftMotorTimer;
+
+void rightMotorCallback(void const *argument);
+osTimerDef (rightMotorDef, rightMotorCallback);
+osTimerId rightMotorTimer;
+
 
 /*
- * main: initialize and start the system
+ * @Brief Thread to receive instructions from the LCD board
  */
-int main (void) {
+void receiverThreadDef(void const *argument){
 	// Enable transmission on the CC2500
 	init_SPI1();
 	uint8_t statusByte;
@@ -36,7 +68,7 @@ int main (void) {
 	statusByte = CC2500_Start_Receive();
 	while((statusByte & 0xF0) != RECEIVING){
 		statusByte = CC2500_No_Op();
-		printf("Status: %02x\n", statusByte);
+		//printf("Status: %02x\n", statusByte);
 	}
 	
 	uint8_t bytesAvailable;
@@ -46,7 +78,7 @@ int main (void) {
 		
 		// Check that the receiver is in the receiving state
 		while((statusByte & 0xF0) == RECEIVING){
-			statusByte = CC2500_Read(&bytesAvailable, BYTES_AVAILABLE_REG, 2);
+			statusByte = CC2500_Read(&bytesAvailable, RX_BYTES, 2);
 			
 			// If data is available, print it
 			if(bytesAvailable > 0){
@@ -67,4 +99,74 @@ int main (void) {
 		}
 		
 	}
+	
+}
+
+// Left motor thread
+static int left_angle;
+void leftMotorThreadDef(void const *argument){
+	while(1){
+		osSignalWait(LEFT_MOTOR_FLAG, osWaitForever);
+		
+		TIM_SetCompare1(LEFT_MOTOR, left_angle);
+		
+		osSignalClear(leftMotorThread, LEFT_MOTOR_FLAG);
+	}
+}
+
+// Right motor thread
+static int right_angle;
+void rightMotorThreadDef(void const *argument){
+	while(1){
+		osSignalWait(RIGHT_MOTOR_FLAG, osWaitForever);
+		
+		
+		TIM_SetCompare1(RIGHT_MOTOR, right_angle);
+		
+		
+		osSignalClear(rightMotorThread, RIGHT_MOTOR_FLAG);
+	}
+}
+
+
+/*
+ * main: initialize and start the system
+ */
+int main (void) {
+	osKernelInitialize();
+	
+	servo_init();
+	
+	osDelay(10000);
+	getAngles(&left_angle, &right_angle, 0, 150);
+	
+	leftMotorThread = osThreadCreate(osThread(leftMotorThreadDef), NULL);
+	rightMotorThread = osThreadCreate(osThread(rightMotorThreadDef), NULL);
+	
+	uint32_t leftMotorTimerType = 1;
+	leftMotorTimer = osTimerCreate (osTimer(leftMotorDef), osTimerPeriodic, &leftMotorTimerType);
+	
+	leftMotorDelay = 10;
+	osTimerStart (leftMotorTimer, leftMotorDelay); 
+	
+	uint32_t rightMotorTimerType = 1;
+	rightMotorTimer = osTimerCreate (osTimer(rightMotorDef), osTimerPeriodic, &rightMotorTimerType);
+	
+	rightMotorDelay = 10;
+	osTimerStart (rightMotorTimer, rightMotorDelay); 
+	
+	receiverThread = osThreadCreate(osThread(receiverThreadDef), NULL);
+	
+	osKernelStart();
+	
+	osDelay(10000);
+	getAngles(&left_angle, &right_angle, 0, 50);
+}
+
+void leftMotorCallback(void const *argument){
+	osSignalSet(leftMotorThread, LEFT_MOTOR_FLAG);
+}
+
+void rightMotorCallback(void const *argument){
+	osSignalSet(rightMotorThread, RIGHT_MOTOR_FLAG);
 }
