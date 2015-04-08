0c2cae308f175159dbf6aa528b5ca67c247d054d
Ryan Singzon <rcsingzon@gmail.com>
2015-04-08 17:04:34 -0400

    Working on keypad debouncing

--- lcd/src/commands.c
+++ lcd/src/commands.c
@@ -15,20 +15,11 @@
 uint8_t sendShape(uint8_t shape){
 	uint8_t statusByte;
 	uint8_t message;
-	uint8_t bigMessage[8];
 	
 	// Send a message containing the shape being sent
 	switch(shape){
 		case TRIANGLE:
 			message = TRIANGLE_COMMAND;
-			bigMessage[0] = 20;
-			bigMessage[1] = 21;
-			bigMessage[2] = 22;
-			bigMessage[3] = 23;
-			bigMessage[4] = 24;
-			bigMessage[5] = 25;
-			bigMessage[6] = 26;
-			bigMessage[7] = 27;
 			break;
 		
 		case SQUARE:
@@ -40,27 +31,22 @@ uint8_t sendShape(uint8_t shape){
 			break;
 	}
 	
-	//statusByte = CC2500_Write(&message, TX_FIFO_BYTE_ADDRESS , 1);	
-	statusByte = CC2500_Write(bigMessage, TX_FIFO_BURST_ADDRESS, 8);
-	
+	statusByte = CC2500_Write(&message, TX_FIFO_BYTE_ADDRESS , 1);		
 	return statusByte;
 }
 
 /**
 * @brief Sends a command to draw a free drawing
-* @param list of directions to draw
+* @param line to draw
 * @retval Status byte
 */
-uint8_t sendFreeDraw(uint8_t *directions){
+uint8_t sendFreeDraw(uint8_t direction){
 	uint8_t statusByte;
 	uint8_t message;
 	int count = 0;
 	
-	// Iterate through the directions in the buffer 
-	while(directions[count] != 0x00){
-		message = directions[count];
-		statusByte = CC2500_Write(&message, TX_FIFO_BYTE_ADDRESS, 1);
-	}
+	message = direction;
+	statusByte = CC2500_Write(&message, TX_FIFO_BYTE_ADDRESS, 1);
 	
 	return statusByte;
 }
